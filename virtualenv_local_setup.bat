ffb76c7cbf1d6b4970c6c25f79c3c7682a3aa035
afabiani <alessio.fabiani@geo-solutions.it>
2017-02-28 14:38:12 +0100

    - fix virtualenv_local_setup issue

--- virtualenv_local_setup.bat
+++ virtualenv_local_setup.bat
@@ -3,13 +3,13 @@ echo Setup python Virtualenv
 
 call python_env.bat
 
-pip install --no-index -I pip-9.0.1-py2.py3-none-any.whl
 pip install --no-index -I virtualenv-13.1.2-py2.py3-none-any.whl
 
 virtualenv .
 
 call Scripts\activate.bat
 
+pip install --no-index -I pip-9.0.1-py2.py3-none-any.whl
 pip install --no-index -I Paver-1.2.4-py2.py3-none-any.whl
 
 pip install -r requirements.txt
